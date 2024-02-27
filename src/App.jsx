60349854f4a6c51a47532aa987f13af53dfd995e
Sam-Kikoma <kikomasamson@gmail.com>
2024-02-27 17:15:52 +0100

    Prod build

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Navbar from "./components/Navbar";
 import "./index.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 const App = () => {
 	return (
 		<>
-			<Navbar />
+			<header id="header" className="bg-cover">
+				<Navbar />
+				<Hero />
+			</header>
+			<Portfolio />
+			<About />
+			<Contact />
 		</>
 	);
 };
